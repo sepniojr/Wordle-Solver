f7dcc8a9b72bf52ba2a778354483a76e0023bf9f
sepniojr <sepniojr@ualberta.ca>
2023-12-02 18:33:22 -0700

    init cells function

--- main.py
+++ main.py
@@ -29,13 +29,23 @@ This can be done in a preprocessing step
 If two letters are consecutive in the word then we set constraints so that the other variables beside the consecutive letters cannot be the same letter
 (e.g. the letters in the word are _EE__ we can remove E from the domain of the first and fourth letters)
 
+
+This problem differs from Sudoku in that any word within the list of valid Wordle words represents a solution to the problem, so there may be multiple solutions for the same constraints.
+However, we are trying to help solve the Wordle, so it is in our best interest to try and search for the best possible solution.
+
 How to select the next variable?
-Choose the next variable based on whichever slot has the smallest domain.
+    - Choose the next variable based on whichever slot has the smallest domain. (MRV selector)
 
 How to select the next value for that variable?
+    - We should prioritize any letters that we know are present in the word (e.g. Yellow letters) (How to do this?) (Set up priority queue of letters?)
+    - Look at the domain of the variable and select the letter with the highest frequency in that slot of all valid Wordle words.
+    - Possible sol'n: Organize the domain list of each variable and order it by Yellow letters first and then go down by letter frequency in that slot
+        - Need a function that checks the yellow letters list before each search and moves the yellow letters to the front of the domain string
+        - This function also indexes the list of letter frequency by position and orders the rest of the letters that way
 
 
-Any word within the list of valid Wordle words represents a solution to the problem, so there may be multiple solutions for the same constraints.
+
+TODO: Important questions to answer: What happens if the user enters a word and it's all wrong? How do we decide which word to recommend them?
 """
 valid_words = []
 
@@ -43,12 +53,44 @@ class Word:
     """
      Class to represent a 5-letter word and an assignment of letters to each variable in the word.
     
-     Variable _cells stores a matrix with 5 entries, one for each variable in the puzzle
+     Variable _domains stores a matrix with 5 entries, one for each variable in the puzzle. 
+     Each entry of the matrix stores the domain of a variable. Initially, the domain of each variable is all letters of the English alphabet, ABCDEFGHIJKLMNOPQRSTUVWXYZ
+     When the user enters their first guess, the domains of the variables are adjusted to fit their corresponding constraints
     """
     def __init__(self):
         self._width = 5
+        self._domains = []
         self._cells = []
-    
+        self._yellow_letters = []
+        self._complete_domain = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+    def init_cells(self, word_string, result_string):
+        """
+        This function takes in the word the user guessed and the result of that word and sets the initial domain of each cell accordingly
+        This function is only called once; Further modifications to the domain will be done in a separate consistency function
+
+        If a letter in a guessed word is grey, that letter is removed from the domain of the variable
+        If a letter in a guessed word is yellow, that letter is removed from the domain of the variable but put into a yellow letter list for later
+        If a letter in a guessed word is green, the domain of the variable is set to that letter
+        """
+        word = list(word_string)
+        result = list(result_string)
+        row = []
+
+        for i in range(len(word_string)):
+            if result[i] == 'X':
+                row.append(self._complete_domain.replace(word[i], ''))
+            elif result[i] == 'Y':
+                row.append(self._complete_domain.replace(word[i], ''))
+                self._yellow_letters.append(word[i])
+            elif result[i] == 'G':
+                row.append(word[i])
+
+        self._cells.append(row)
+        pass
+        
+
     def copy(self):
         copy_word = Word()
         copy_word._cells = self._cells.copy()
@@ -59,9 +101,6 @@ class Word:
     
     def get_width(self):
         return self._width
-    
-    def get_cell_string(self):
-        return(''.join(self._cells))
 
     def is_solution(self):
         if self.get_cell_string in valid_words:
@@ -98,7 +137,7 @@ def is_valid_word(guessed_word):
     if len(guessed_word) != 5:
         print("Please guess a 5-letter word\n")
         return False
-    elif guessed_word not in valid_words:
+    elif guessed_word.lower() not in valid_words:
         print("Not a valid Wordle word!\n")
         return False
     return True
@@ -109,6 +148,7 @@ if __name__ == "__main__":
     for valid_word in file:
         valid_word = valid_word.strip()
         valid_words.append(valid_word)
+    
 
     while(True):
         guessed_word = input("Please enter the word you guessed: ").upper()
@@ -123,4 +163,7 @@ if __name__ == "__main__":
         if not is_valid_result(result_guessed_word):
             print("NOT A VALID INPUT")
             break
+
+        word = Word()
+        word.init_cells(guessed_word,result_guessed_word)
 
